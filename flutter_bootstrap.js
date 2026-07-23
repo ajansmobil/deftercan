b8fcf564c1cd2443bf7b9699963669b153e2104c
yunusevgane <yunusevgane@users.noreply.github.com>
2026-07-23 15:50:48 +0000

    Deploy: Flutter web release (196100fd503804810fb6ee6f5ef492ffef1f6a70)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3838723712" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2515484249" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
